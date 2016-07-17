c5993eade9a0c1c010b554f7961451e87078793d
Jackson Yeager <jacksonyeager@gmail.com>
2016-07-17 14:03:18 -0400

    Navbar Update
    Navbar now becomes opaque when scrolled past a certain area

--- js/volition.js
+++ js/volition.js
@@ -1,16 +1,55 @@
-$(document).ready(function(){
-  $("a").on('click', function(event) {
-    if (this.hash !== "") {
-      event.preventDefault();
+$(document).ready(function () {
+    var transparent = true;
+    $("a").on('click', function (event) {
+        if (this.hash !== "") {
+            event.preventDefault();
+            var hash = this.hash;
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top
+            }, 800, function () {
+                window.location.hash = hash;
+            });
+        }
+    });
+    
+    $(window).scroll(function () {
+        checkForNav();
+    });
+    
+    var checkForNav = debounce(function () {
+        if ($(document).scrollTop() > 560) {
+            if (transparent) {
+                transparent = false;
+                $('nav[role="navigation"]').addClass('navbar-solid');
+            }
+        }
+        else {
+            if (!transparent) {
+                transparent = true;
+                $('nav[role="navigation"]').removeClass('navbar-solid');
+            }
+        }
+    }, 17);
 
-      var hash = this.hash;
+    function debounce(func, wait, immediate) {
+        var timeout;
+        return function () {
+            var context = this
+                , args = arguments;
+            clearTimeout(timeout);
+            timeout = setTimeout(function () {
+                timeout = null;
+                if (!immediate) func.apply(context, args);
+            }, wait);
+            if (immediate && !timeout) func.apply(context, args);
+        };
+    };
 
-      $('html, body').animate({
-        scrollTop: $(hash).offset().top
-      }, 800, function(){
-   
-        window.location.hash = hash;
-      });
+    function inView(elem) {
+        var docViewTop = $(window).scrollTop();
+        var docViewBottom = docViewTop + $(window).height();
+        var elemTop = $(elem).offset().top;
+        var elemBottom = elemTop + $(elem).height();
+        return ((elemBottom >= docViewTop) && (elemTop <= docViewBottom) && (elemBottom <= docViewBottom) && (elemTop >= docViewTop));
     }
-  });
 });
